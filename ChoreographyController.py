df6022352c03f125cac1c31a4e1ba8d8974074a1
Frantz P <frantz.prochaska@uni-a.de>
2024-08-05 11:23:47 +0200

    fds

--- ChoreographyController.py
+++ ChoreographyController.py
@@ -1,49 +1,59 @@
-#!/usr/bin/env python
 import signal
 import sys
-import time
+import pickle
+from threading import Thread
 from time import sleep
 
 from AbstractVirtualCapability import AbstractVirtualCapability, VirtualCapabilityServer, formatPrint
 
 
-class ViconPosition(AbstractVirtualCapability):
+class TestField(AbstractVirtualCapability):
+
     def __init__(self, server):
         super().__init__(server)
-        self.functionality = {"GetViconPosition": None}
+        self.TestFieldBoundaries = [[1, -1, 0.], [-1., 1., 0.]]
+        '''
+        try:
+            self.TestFieldBoundaries = pickle.loads("TestFieldBoundaries")
+        except:
+            pickle.dumps("TestFieldBoundaries", self.TestFieldBoundaries)
+        '''
+        formatPrint(self, f"positions : {self.TestFieldBoundaries}")
 
-    def GetViconPosition(self, params: dict) -> dict:
-        if self.functionality["GetViconPosition"] is not None:
-            vicon_name = params["SimpleStringParameter"]
-            if not isinstance(vicon_name, str):
-                raise TypeError("Parameter of Function has to be a String")
-            pos = self.functionality["GetViconPosition"](vicon_name)
-            return {"Position3D": pos}
-        else:
-            return {"Position3D": [0., 0., 0.]}
+    def GetBuildingLights(self, params: dict) -> dict:
+        #formatPrint(self, f"Sending TestFieldBountaries: {self.TestFieldBoundaries}")
+        return {"TestFieldPointA": self.TestFieldBoundaries[0],
+                "TestFieldPointB": self.TestFieldBoundaries[1]}
+
+    def SetBuildingLights(self, params: dict) -> dict:
+        self.TestFieldBoundaries[0] = params["TestFieldPointA"]
+        self.TestFieldBoundaries[1] = params["TestFieldPointB"]
+        pickle.dump("TestFieldBoundaries", self.TestFieldBoundaries)
+        return self.GetTestFieldBoundaries(params)
+        
+    def updateCopter(self, params: dict) -> dict:
+        self.TestFieldBoundaries[0] = params["TestFieldPointA"]
+        self.TestFieldBoundaries[1] = params["TestFieldPointB"]
+        pickle.dump("TestFieldBoundaries", self.TestFieldBoundaries)
+        return self.GetTestFieldBoundaries(params)
+
 
     def loop(self):
         pass
-        
-if __name__ == '__main__':
-    # Needed for properly closing when process is being stopped with SIGTERM signal
-    def handler(signum, frame):
-        print("[Main] Received SIGTERM signal")
-        listener.kill()
-        quit(1)
 
 
+if __name__ == "__main__":
     try:
         port = None
         if len(sys.argv[1:]) > 0:
             port = int(sys.argv[1])
         server = VirtualCapabilityServer(port)
-        listener = ViconPosition(server)
-        listener.start()
-        signal.signal(signal.SIGTERM, handler)
-        listener.join()
-    # Needed for properly closing, when program is being stopped wit a Keyboard Interrupt
+        tf = TestField(server)
+        tf.start()
+        while server.running:
+            pass
+        # Needed for properly closing, when program is being stopped wit a Keyboard Interrupt
     except KeyboardInterrupt:
         print("[Main] Received KeyboardInterrupt")
-        server.kill()
-        listener.kill()
+
+
